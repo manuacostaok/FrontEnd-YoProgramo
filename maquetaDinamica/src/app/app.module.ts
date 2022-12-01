f465fab62088fed20a77c6236aea1ac3b3b05178
manuacostaok <manuacostaok@gmail.com>
2022-12-01 20:01:00 -0300

    maqueta dinamica v1

--- maquetaDinamica/src/app/app.module.ts
+++ maquetaDinamica/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './componentes/footer/footer.component';
 import { HeaderComponent } from './componentes/header/header.component';
+import { LogoComponent } from './componentes/logo/logo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    FooterComponent,
     HeaderComponent,
-    FooterComponent
+    LogoComponent
   ],
   imports: [
     BrowserModule,

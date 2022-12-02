39212010496ba94fa2b317a5982bbd2babd95976
manuacostaok <manuacostaok@gmail.com>
2022-12-02 04:31:21 -0300

    boton login con funcion mostrar

--- maquetaDinamica/src/app/componentes/navbar/navbar.component.ts
+++ maquetaDinamica/src/app/componentes/navbar/navbar.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DatosService } from 'src/app/servicios/datos.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+    
+  redes:any;
+  mostrar = false;
+  constructor(private datos: DatosService){}
+  ngOnInit():void{
+      
+      
+    }
+  
+
+  mostrate():void{
+    this.mostrar=!this.mostrar;
+  }
+
 
 }

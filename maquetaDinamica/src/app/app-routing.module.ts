c87a8af87d2b713cfd32f0c24ac10d2db0265f59
manuacostaok <manuacostaok@gmail.com>
2022-12-02 00:23:27 -0300

    maq dinamica v1.1

--- maquetaDinamica/src/app/app-routing.module.ts
+++ maquetaDinamica/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './componentes/index/index.component';
+import { Pagina404Component } from './componentes/pagina404/pagina404.component';
 
-const routes: Routes = [];
+//las rutas van ordenandas, al final va el error, primero el index 
+const routes: Routes = [
+  {path:'', component: IndexComponent},
+  {path:'**', component: Pagina404Component}
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- maquetaDinamica/src/app/app.module.ts
+++ maquetaDinamica/src/app/app.module.ts
@@ -6,13 +6,35 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './componentes/footer/footer.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { LogoComponent } from './componentes/logo/logo.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { IndexComponent } from './componentes/index/index.component';
+import { Pagina404Component } from './componentes/pagina404/pagina404.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { SobreMiComponent } from './componentes/sobre-mi/sobre-mi.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { LogoutComponent } from './componentes/logout/logout.component';
+import { ModalLoginComponent } from './modales/modal-login/modal-login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    LogoComponent 
+    LogoComponent,
+    NavbarComponent,
+    EducacionComponent,
+    ExperienciaComponent,
+    HabilidadesComponent,
+    IndexComponent,
+    Pagina404Component,
+    ProyectosComponent,
+    SobreMiComponent,
+    LoginComponent,
+    LogoutComponent,
+    ModalLoginComponent 
   ],
   imports: [
     BrowserModule,

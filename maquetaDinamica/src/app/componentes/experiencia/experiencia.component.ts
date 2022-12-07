9bf3466a474e9ea02822e09bf8fb13fa69556d99
manuacostaok <manuacostaok@gmail.com>
2022-12-07 18:31:30 -0300

    mejoras

--- maquetaDinamica/src/app/componentes/experiencia/experiencia.component.ts
+++ maquetaDinamica/src/app/componentes/experiencia/experiencia.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DatosService } from 'src/app/servicios/datos.service';
 
 @Component({
   selector: 'app-experiencia',
   templateUrl: './experiencia.component.html',
   styleUrls: ['./experiencia.component.css']
 })
-export class ExperienciaComponent {
+export class ExperienciaComponent implements OnInit{
+trabajitos:any=[]; //instanciamos las variables para usarlas en el metedo ngOnInit
+nombrecito:string='';
+  constructor(private datos:DatosService){ }
+  ngOnInit(): void{
+    this.datos.getDatos().subscribe(data =>{
+      this.trabajitos=data.trabajos;
+      this.nombrecito=data.nombre;
+    })
+  }
 
 }
+

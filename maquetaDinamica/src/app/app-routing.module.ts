78889d3fb0f28c69ab4a8bc61f2459941cea398b
manuacostaok <manuacostaok@gmail.com>
2022-12-08 00:49:50 -0300

    act v2.1

--- maquetaDinamica/src/app/app-routing.module.ts
+++ maquetaDinamica/src/app/app-routing.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
 import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
 import { IndexComponent } from './componentes/index/index.component';
 import { Pagina404Component } from './componentes/pagina404/pagina404.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
 
 //las rutas van ordenandas, al final va el error, primero el index 
 const routes: Routes = [
   {path:'', component: IndexComponent},
   {path:'experiencia', component: ExperienciaComponent},
+  {path:'estudios', component: EducacionComponent},
+  {path:'habilidades', component: HabilidadesComponent},
+  {path:'proyectos', component: ProyectosComponent},
   {path:'**', component: Pagina404Component}
   
 

38ef0e42ffdd1742d58b2f16314d6e5f8f7c1544
manuacostaok <manuacostaok@gmail.com>
2023-01-09 01:12:10 -0300

    avances en el back 2

--- maquetaDinamica/src/app/entidades/persona.ts
+++ maquetaDinamica/src/app/entidades/persona.ts
@@ -4,17 +4,17 @@ export class Persona {
     apellido: string;
     img: string;
     titulo: string;
-    acercaDe: string;
+    sobreMi: string;
     banner: string;
     email: string;
     curriculum: string;
 
-    constructor(nombre: string, apellido: string, img: string, titulo: string, acercaDe: string, banner: string, email: string, curriculum: string){
+    constructor(nombre: string, apellido: string, img: string, titulo: string, sobreMi: string, banner: string, email: string, curriculum: string){
         this.nombre = nombre;
         this.apellido = apellido;
         this.img = img;
         this.titulo = titulo;
-        this.acercaDe = acercaDe;
+        this.sobreMi = sobreMi;
         this.banner = banner;
         this.email = email;
         this.curriculum = curriculum;
